04d8514cd3d29111d108510bad5d739fddd22bac
Glauber Ferreira <glauberdnb@gmail.com>
2025-09-26 13:04:33 +0000

    mudar o logo do snooker bar e colocar uma bola de snooker

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -7,21 +7,16 @@ export const SnookerBarLogo = (props: React.SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path fill="none" d="M0 0h256v256H0z" />
-    <path
-      d="M48.2 188.39a8 8 0 0 0 11.23-1.22l57.26-78.78a32 32 0 1 1 54.62 39.88L114.1 228.39a8 8 0 0 1-13.86-8l27-46.8"
+    <circle
+      cx="128"
+      cy="128"
+      r="96"
+      fill="none"
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={24}
-      fill="none"
     />
-    <path
-      d="M110.19 110.15a32.07 32.07 0 0 1 45.21-.11l57.34 78.85a8 8 0 0 1-12.63 9.24l-57.34-78.85a32.07 32.07 0 0 0-45.21.11"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={24}
-      fill="none"
-    />
+    <circle cx="128" cy="128" r="40" fill="currentColor" />
   </svg>
 );
