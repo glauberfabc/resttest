e3d5c162f77204d586bdfbf50a8dece524cb796f
Glauber Ferreira <glauberdnb@gmail.com>
2025-10-02 12:27:20 +0000

    Console Error
    Error fetching profile after login: "TypeError: Failed t

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 import type { Order, MenuItem, Client } from './types';
+require('dotenv').config();
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
