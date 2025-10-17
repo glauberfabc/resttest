0b167ee28ee6b39550aac4709910af9f974d0293
Glauber Ferreira <glauberdnb@gmail.com>
2025-10-17 12:13:41 +0000

    Error: Route "/dashboard" used `cookies().get('sb-zzbxhgqjasddlrpyrnvo-a

--- src/lib/user-actions.ts
+++ src/lib/user-actions.ts
@@ -1,5 +1,5 @@
 
-import { createServerClient } from '@supabase/ssr'
+import { type CookieOptions, createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import type { User } from '@/lib/types';
 import { redirect } from 'next/navigation';
@@ -14,6 +14,24 @@ export async function getCurrentUser(): Promise<User | null> {
             cookies: {
                 get(name: string) {
                     return cookieStore.get(name)?.value
+                },
+                set(name: string, value: string, options: CookieOptions) {
+                    try {
+                        cookieStore.set({ name, value, ...options })
+                    } catch (error) {
+                        // The `set` method was called from a Server Component.
+                        // This can be ignored if you have middleware refreshing
+                        // user sessions.
+                    }
+                },
+                remove(name: string, options: CookieOptions) {
+                    try {
+                        cookieStore.set({ name, value: '', ...options })
+                    } catch (error) {
+                        // The `delete` method was called from a Server Component.
+                        // This can be ignored if you have middleware refreshing
+                        // user sessions.
+                    }
                 },
             },
         }
